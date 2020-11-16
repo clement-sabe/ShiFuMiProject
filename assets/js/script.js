c6a5ad275c59dfb4a4c971b1a69d77a768a72981
clément-sabe <clem.sabe@gmail.com>
2020-11-16 16:59:20 +0100

    sound

--- assets/js/script.js
+++ assets/js/script.js
@@ -114,3 +114,22 @@ $( "#emptyBlock" ).droppable({
   }
 });
 });
+
+
+$(document).ready(function(){
+	$(".toggle").click(function(){
+	$(this).toggleClass("active");
+	});
+
+	$(".slide-body").click(function(){
+	$(".slide-body .slide").toggleClass("active");
+	});
+
+  $(".on").click(function(){
+    $("#audio-player")[0].play();
+  });
+
+  $(".off").click(function(){
+    $("#audio-player")[0].pause();
+  });
+});
